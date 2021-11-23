0a1ed666d8edc59f1b1fb5828d5b925d26cb3b45
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2021-11-23 13:38:56 +0530

    link sponsorship brochure

--- src/components/Sponsors/sponsors.jsx
+++ src/components/Sponsors/sponsors.jsx
@@ -9,9 +9,11 @@ function SponsorsHead() {
 
 function Sponsor(props) {
   return (
-    <div className="Sponsor ">
-      <img src={props.srcx} alt="Dechacks"></img>
-    </div>
+    <a href="sponsorship_gdscsnu.pdf" target="__blank">
+      <div className="Sponsor ">
+        <img src={props.srcx} alt="Dechacks"></img>
+      </div>
+    </a>
   );
 }
 
