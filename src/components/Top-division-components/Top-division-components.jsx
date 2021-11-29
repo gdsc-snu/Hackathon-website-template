41035bf4b2b7c6df000321c9b26fa04b06312d74
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2021-11-29 23:32:10 +0530

    fix devfolio sdk src

--- src/components/Top-division-components/Top-division-components.jsx
+++ src/components/Top-division-components/Top-division-components.jsx
@@ -28,7 +28,7 @@ function About() {
 function Myinfo() {
   React.useEffect(() => {
     const script = document.createElement('script');
-    script.src = '<https://apply.devfolio.co/v2/sdk.js>';
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
